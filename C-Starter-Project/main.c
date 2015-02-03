3787abba4d694a60131667159ca2a6dce67eef0f
dannylmathews <dannyl.mathews@gmail.com>
2015-02-02 16:30:26 -0800

    Initial Commit

---
+++ C-Starter-Project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C-Starter-Project
+//
+//  Created by Daniel Mathews on 2015-02-02.
+//  Copyright (c) 2015 com.lighthouse-labs. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
